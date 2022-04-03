c6363cbed94585ab0de8251227bae3bfbdf96197
Filip <filip7243@gmail.com>
2022-04-03 15:58:22 +0200

    Added Lab4.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
 
+    //LAB1 LAB2
     public static void main(String[] args) {
         //KOD DO OSTATNIEGO ZADANIA Z LAB1:
         Random random = new Random();
