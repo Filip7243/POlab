e00b1fce39e90867e03ba59d4522a77a33d93e00
Filip <ur@example.com>
2022-04-06 14:20:35 +0200

    Added class Main and done Rectangle

--- src/com/Lab5/Rectangle.java
+++ src/com/Lab5/Rectangle.java
@@ -1,4 +1,25 @@
 package com.Lab5;
 
 public class Rectangle {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    private double getCircuit() {
+        return (this.width * 2) + (this.height * 2);
+    }
+
+    private double getDiagonal() {
+        return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    }
+
 }
