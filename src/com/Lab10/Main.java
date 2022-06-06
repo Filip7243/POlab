4e5efc2ce60d4d3e5424e5d1297706389aaae005
Filip <filip7243@gmail.com>
2022-06-06 22:54:48 +0200

    almost done everything

--- src/com/Lab10/Main.java
+++ src/com/Lab10/Main.java
@@ -12,10 +12,10 @@ public class Main {
 
         // dla kalsy Kangur
 
-        List<Kangur> kangury = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            kangury.add(new Kangur(i));
-        }
+//        List<Kangur> kangury = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            kangury.add(new Kangur(i));
+//        }
 
 //        for(int i = 0; i < kangury.size(); i++) {
 //            kangury.get(i).skok();
@@ -26,13 +26,13 @@ public class Main {
 //            i.next().skok();
 //        }
 
-        Map<String, Kangur> kangurMap = new HashMap<>();
-        int j = 0;
-        for (Kangur k : kangury) {
-            kangurMap.put("Jan" + j, k);
-            j++;
-        }
-        kangurMap.put("Ala0", new Kangur(1111));
+//        Map<String, Kangur> kangurMap = new HashMap<>();
+//        int j = 0;
+//        for (Kangur k : kangury) {
+//            kangurMap.put("Jan" + j, k);
+//            j++;
+//        }
+//        kangurMap.put("Ala0", new Kangur(1111));
 
 //        Iterator<String> iMap = kangurMap.keySet().iterator();
 //        while(iMap.hasNext()) {
@@ -41,14 +41,45 @@ public class Main {
 //            kangurMap.get(name).skok();
 //        }
 
-        TreeMap<String, Kangur> treeKangur = new TreeMap<>(kangurMap);
-        LinkedHashMap<String, Kangur> kangurLinkedHashMap = new LinkedHashMap<>();
+//        TreeMap<String, Kangur> treeKangur = new TreeMap<>(kangurMap);
+//        LinkedHashMap<String, Kangur> kangurLinkedHashMap = new LinkedHashMap<>();
+//
+//        for(Map.Entry<String, Kangur> entry : treeKangur.entrySet()) {
+//            String name = entry.getKey();
+//            Kangur kangur = entry.getValue();
+//            kangurLinkedHashMap.put(name, kangur);
+//        }
 
-        for(Map.Entry<String, Kangur> entry : treeKangur.entrySet()) {
-            String name = entry.getKey();
-            Kangur kangur = entry.getValue();
-            kangurLinkedHashMap.put(name, kangur);
+        // dla klasy Stos
+
+        Stos<String> stos = new Stos<>();
+        String s = "+B+a+ł---+a+g+a---+n-+w-+l+i+t---+e-+r+k--+a+c+h---";
+        String[] sTab = s.split("");
+        for (int i = sTab.length - 1; i >= 0; i--) {
+            stos.push(sTab[i]);
         }
+
+        int pluses = 0;
+        int minuses = 0;
+        while (!stos.isEmpty()) {
+            if(stos.peek().equals("+")) {
+                pluses += 1;
+                stos.pop();
+                continue;
+            }
+            for(int i = 0; i < pluses; i++) {
+                System.out.println(stos.pop());
+            }
+            if(stos.peek().equals("-")) {
+                minuses += 1;
+                stos.pop();
+                continue;
+            }
+            for(int i = 0; i < minuses; i++) {
+                System.out.println(stos.pop());
+            }
+        }
+
 
     }
 
---
+++ src/com/Lab10/Stos.java
@@ -0,0 +1,31 @@
+package com.Lab10;
+
+import java.util.LinkedList;
+
+public class Stos<T> {
+
+    private LinkedList<T> stos = new LinkedList<>();
+
+    public void push(T v) {
+        stos.push(v);
+    }
+
+    public T peek() {
+        return stos.peek();
+    }
+
+    public T pop() {
+        return stos.pop();
+    }
+
+    public boolean isEmpty() {
+        return stos.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "Stos{" +
+                "stos=" + stos +
+                '}';
+    }
+}
